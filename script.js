9030c28a0aae223d5846556b717b3938467feb7e
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2021-03-29 12:38:30 -0300

    muitas mudancas no html da pagina de pa e script funcionando certinho

--- script.js
+++ script.js
@@ -431,28 +431,93 @@ Atenciosamente,
 
 //Final da página de Consignações
 
-//Início da página de P.A
+//Início da página de Pensão Alimentícia
 
 function respostaPA() {
-  const processo = document.querySelector("#PA_processo").value;
+  const processo = document.querySelector("#pa_processo").value;
 
-  const segurado = document.querySelector("#PA_segurado_nome").value;
+  const instituidor = document.querySelector("#nome_inst").value;
 
-  const tarefa = document.querySelector("#tarefa").value;
+  const dependente1 = document.querySelector("#nome_dep_1").value;
 
-  // let situacao = document.getElementsByClassName("radio");
+  const tarefa = document.querySelector("#pa_tarefa").value;
 
-  let resposta = document.getElementById("vinculos_textarea");
+  const situacao = document.getElementsByClassName("radio_pa");
 
-  let assunto = document.getElementById("vinculos_assunto_email");
+  const nbImplantado = document.querySelector("#pa_nbImplantado").value;
 
-  assunto.innerHTML = `Ofício - Processo ${processo} - ${segurado}`;
 
-  resposta.innerHTML = `Em atenção ao disposto no ofício relacionado ao processo ${processo}, informamos que foi aberta a tarefa de nº ${tarefa} para implantação da referida Pensão Alimentícia. O andamento deste protocolo poderá ser acompanhado pelo(s) interessado(s) através do site gov.br/inss, do aplicativo 'Meu INSS' ou através do telefone 135.
-        
-Atenciosamente,
-        
-        `;
+
+//variáveis relacionadas à parte das respostas
+  const resposta = document.getElementById("pa_textarea");
+
+  const assunto = document.getElementById("pa_assunto_email");
+
+  const atenciosamente = `Atenciosamente, 
+  `
+
+  //Frases em variáveis para melhor organização;
+
+  const faltaCpfTitular = `Em atenção ao disposto no ofício relacionado ao processo ${processo}, informamos que não foi possível realizar a abertura da tarefa para implantação da referida Pensão Alimentícia no benefício do instituidor ${instituidor}, tendo em vista que é obrigatório informar o número de CPF do titular ${dependente1}. 
+  
+Sendo assim, solicitamos que, se possível, este dado seja enviado para este e-mail para que a presente demanda possa ser cumprida de forma mais célere. \n\n${atenciosamente}`
+
+  const faltaOutraCoisa = `Falta outra coisa`;
+
+  const faltaOutraOutraCoisa = `Falta outra outra coisa`;
+
+  const ddb = document.querySelector("#pa_ddb").value;
+
+  const tarefaAberta = `Em atenção ao disposto no ofício relacionado ao processo ${processo}, informamos que foi aberta a tarefa de nº ${tarefa} para implantação da referida Pensão Alimentícia. O andamento deste protocolo poderá ser acompanhado pelo(s) interessado(s) através do site gov.br/inss, do aplicativo 'Meu INSS' ou através do telefone 135.`
+  
+  const paJaImplantada = `Em atenção ao disposto no ofício relacionado ao processo ${processo}, informamos que a referida Pensão Alimentícia no benefício do instituidor ${instituidor} foi implantada sob o número ${nbImplantado} desde ${ddb} conforme informações constantes no relatório anexo. \n\n${atenciosamente}`
+  
+  const implantadaAgora = `Em atenção ao disposto no ofício relacionado ao processo ${processo}, informamos que a referida Pensão Alimentícia foi implantada agora...`
+  
+  const outraSituacao = `Outra situação`
+
+  assunto.innerHTML = `Ofício - Processo ${processo} - ${instituidor}`;
+
+  // Fim das frases - Início dos radios inputs
+
+  if(situacao[0].checked){
+    resposta.innerHTML = faltaCpfTitular;
+
+  } else if(situacao[1].checked){
+    resposta.innerHTML = faltaOutraCoisa;
+
+  } else if(situacao[2].checked){
+    resposta.innerHTML = faltaOutraOutraCoisa;
+
+  } else if(situacao[3].checked){
+    resposta.innerHTML = tarefaAberta;
+
+  } else if(situacao[4].checked){
+    resposta.innerHTML = paJaImplantada;
+
+  } else if(situacao[5].checked){
+    resposta.innerHTML = implantadaAgora;
+
+  } else if(situacao[6].checked){
+    resposta.innerHTML = outraSituacao;
+  }
+  
+// Fim dos radio inputs
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
 }
 
 //Final da página de P.A
@@ -567,3 +632,8 @@ function resposta_protocolo() {
 }
 
 // Fim da página de Procotolo
+
+
+const teste =()=> {
+  return "beatiful text and colors"
+}
