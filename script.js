ed778bfc0df210df32b82850a30b5e62864450b7
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2021-03-30 12:26:39 -0300

    pagina ms refatorada e com algumas opcoes a mais

--- script.js
+++ script.js
@@ -525,62 +525,91 @@ Sendo assim, solicitamos que, se possível, este dado seja enviado para este e-m
 //Início do Script de Mandado de Segurança
 
 function resposta_ms() {
-  let nome = document.querySelector("#ms_nome").value;
+  const nome = document.querySelector("#ms_nome").value;
 
-  let beneficio = document.querySelector("#ms_nb").value;
+  const beneficio = document.querySelector("#ms_nb").value;
 
-  let situacao = document.getElementsByClassName("radio_ms");
+  const situacao = document.getElementsByClassName("radio_ms");
 
-  let agendamento = document.getElementById("ms_agendamento").value;
+  const agendamento = document.getElementById("ms_agendamento").value;
 
-  let horario = document.querySelector("#ms_horario").value;
+  const horario = document.querySelector("#ms_horario").value;
 
-  let aps = document.querySelector("#ms_aps").value;
+  const aps = document.querySelector("#ms_aps").value;
 
-  let resposta = document.querySelector("#ms_textarea");
+  const resposta = document.querySelector("#ms_textarea");
 
-  //Problema no PP
+  //Respostas em variáveis para melhor organização
 
-  if (situacao[0].checked) {
-    resposta.innerHTML = `Encaminhamento de determinação judicial para cumprimento.
+  const erroPP = `Encaminhamento de determinação judicial para cumprimento.
 
 1. Trata-se de Mandado de Segurança impetrado pelo(a) segurado(a) ${nome} que não conseguiu realizar o agendamento de Perícia de Prorrogação dentro do prazo e teve seu benefício nº ${beneficio} indevidamente cessado. 
-  
+    
 2. Situação resolvida e perícia agendada para o dia ${agendamento} às ${horario} na APS ${aps}.
-  
-3. O benefício foi reativado e já consta o cálculo dos pagamentos pendentes conforme relatório anexo. `;
+    
+3. O benefício foi reativado e já consta o cálculo dos pagamentos pendentes conforme relatório anexo.`
 
-    //Finalização de Análise
+
+const finalizarAnalise = `Encaminhamento de determinação judicial para cumprimento.
+
+1. Trata-se de Mandado de Segurança determinando a finalização da análise do benefício 7090285392 em até 30 dias após o cumprimento das exigências por parte do(a) segurado(a) Eliana dos Santos Franca.  
+
+2. A exigência solicitava atualização no Cadúnico, além de outros documentos complementares, e foi realizada em 21/11/2020.
+
+3. Em 15/01/2021 o procurador do(a) segurado(a) fez um pedido de dilação do prazo da exigência.
+
+4. Em 02/03/2021 o requerimento foi indeferido, tendo em vista que não houve manifestação do(a) segurado(a) referente o cumprimento da exigência pendente há mais de 100 dias.
+
+5. Importante ressaltar a ciência do(a) segurado(a) e/ou seu procurador, tendo em vista os constantes acessos ao processo digital registrados no sistema GET.`
+
+
+const informacoesDossie = `Encaminhamento de determinação judicial para cumprimento.
+
+1. Trata-se de Mandado de Segurança impetrado pelo(a) segurado(a) ${nome} e que determina a notificação do INSS para que sejam apresentadas informações que entender necessárias.
+
+2. Sem mais a acrescentar, encaminhamos relatório anexo com consultas diversas aos sistemas do INSS referente a situação relatada.` 
+
+
+const analiseMedica = `Encaminhamento de determinação judicial para cumprimento.
+
+1. Trata-se de Mandado de Segurança impetrado pelo(a) segurado(a) ${nome}. 
+    
+2. É possível verificar através de relatório anexo que a análise médica da Perícia Médica Federal foi de cessação imediata do benefício. 
+    
+3. Nestes casos em que a data de cessação é a própria data da perícia, não é possível agendar prorrogação.
+
+4. Com a cessação do benefício, o segurado tem prazo de 30 dias de prazo para entrar com Recurso Administrativo contra a decisão e/ou terá que aguardar 30 dias para realizar um novo requerimento de benefício.`
+
+
+const outraSituacao = `Outra situação`
+
+
+//Fim das frases - começo da lógica
+
+  if (situacao[0].checked) {
+    resposta.innerHTML = erroPP;
+
   } else if (situacao[1].checked) {
-    resposta.innerHTML = `1. Trata-se de Mandado de Segurança determinando a finalização da análise do benefício 7090285392 em até 30 dias após o cumprimento das exigências por parte do(a) segurado(a) Eliana dos Santos Franca.  
-
-  2. A exigência solicitava atualização no Cadúnico, além de outros documentos complementares, e foi realizada em 21/11/2020.
+    resposta.innerHTML = finalizarAnalise;
   
-  3. Em 15/01/2021 o procurador do(a) segurado(a) fez um pedido de dilação do prazo da exigência.
-  
-  4. Em 02/03/2021 o requerimento foi indeferido, tendo em vista que não houve manifestação do(a) segurado(a) referente o cumprimento da exigência pendente há mais de 100 dias.
-  
-  5. Importante ressaltar a ciência do(a) segurado(a) e/ou seu procurador, tendo em vista os constantes acessos ao processo digital registrados no sistema GET.`;
-
-    //Alguma situação
   } else if (situacao[2].checked) {
-    resposta.innerHTML = `1. Trata-se de Mandado de Segurança solicitando mais informações sobre o caso do(a) segurado(a) ${nome}. determinando a finalização da análise do benefício 7090285392 em até 30 dias após o cumprimento das exigências por parte do(a) segurado(a) Eliana dos Santos Franca.  
+    resposta.innerHTML = informacoesDossie;
 
-  2. A exigência solicitava atualização no Cadúnico, além de outros documentos complementares, e foi realizada em 21/11/2020.
-  
-  3. Em 15/01/2021 o procurador do(a) segurado(a) fez um pedido de dilação do prazo da exigência.
-  
-  4. Em 02/03/2021 o requerimento foi indeferido, tendo em vista que não houve manifestação do(a) segurado(a) referente o cumprimento da exigência pendente há mais de 100 dias.
-  
-  5. Importante ressaltar a ciência do(a) segurado(a) e/ou seu procurador, tendo em vista os constantes acessos ao processo digital registrados no sistema GET.`;
-
-    //Outra situação
   } else if (situacao[3].checked) {
-    resposta.innerHTML = `Outra situação`;
+    resposta.innerHTML = analiseMedica;
+  
+  } else if (situacao[4].checked) {
+    resposta.innerHTML = outraSituacao;
   }
 }
 
 //Final de Mandado de Segurança
+
+
+
+
+
+
 
 //Início da página de Protocolo
 
